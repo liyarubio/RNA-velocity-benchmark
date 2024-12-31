dbfaa7d1a588f483954552965d780d34fa7ea299
Hu PengZhen <68380948+huhuhu129@users.noreply.github.com>
2024-12-31 10:17:33 +0800

    Update 1_sampling.py

--- Scalability/1_sampling.py
+++ Scalability/1_sampling.py
@@ -1,19 +1,14 @@
 import scanpy as sc
 import pandas as pd
+import numpy as np
 
-
-size = [1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,20000,30000,40000,50000,60000]
-
+# load data
 adata = sc.read_h5ad('../data_2000gene/gastrulation.h5ad')
-
-# stratified sampling
-strata = adata.obs['cell_type']
-strata_groups = strata.groupby(strata.columns.tolist())
-
+size = [1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,20000,30000,40000,50000,60000]
+# i = 1000
+time_list = pd.DataFrame()
 for i in size:
-    sampled_adata = []
-    for group_name, group_indices in strata_groups.indices.items():
-        sample_indices = group_indices[:i]
-        sampled_adata.append(adata[sample_indices, :])
-    sampled_adata = sc.concat(sampled_adata)
-    sampled_adata.write_h5ad('../runtime_test/cell_splite/gastrulation_{}cell.h5ad'.format(str(i)))
+    random_indices = np.random.choice(adata.obs_names, i, replace=False)
+    adata1 = adata[random_indices]
+    result_path = './cell_splite/gastrulation_'+str(i)+'cell.h5ad'
+    adata1.write_h5ad(result_path)
